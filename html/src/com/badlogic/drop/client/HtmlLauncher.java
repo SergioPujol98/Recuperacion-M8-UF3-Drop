b55fdc8cb5a8eb66b376e1200ecec7e186beda2c
Black Shadow <sergiopujol.98@gmail.com>
2021-06-07 11:56:53 +0200

    Drop simple

--- html/src/com/badlogic/drop/client/HtmlLauncher.java
+++ html/src/com/badlogic/drop/client/HtmlLauncher.java
@@ -1,18 +1,15 @@
 package com.badlogic.drop.client;
 
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.badlogic.drop.Drop;
 
 public class HtmlLauncher extends GwtApplication {
-
         @Override
         public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(800, 480);
         }
 
         @Override
